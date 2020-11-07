070f402f00fe6e15546f2bc28e49e7dc5c8ab0ae
Martin <hodovanmartin@gmail.com>
2020-11-07 19:28:17 +0100

    Add tickets data

---
+++ src/data/tickets.js
@@ -0,0 +1,220 @@
+export const tickets = {
+  "single-fare-ab": {
+    "name": "Single ticket AB",
+    "zones": "AB",
+    "price": 2.9,
+    "reduced-price": 1.8
+  },
+  "single-fare-bc": {
+    "name": "Single ticket BC",
+    "zones": "BC",
+    "price": 3.3,
+    "reduced-price": 2.3
+  },
+  "single-fare-abc": {
+    "name": "Single ticket ABC",
+    "zones": "ABC",
+    "price": 3.6,
+    "reduced-price": 2.6
+  },
+  "extension-ticket": {
+    "name": "Extension ticket A/C",
+    "zones": "ABC",
+    "price": 1.7,
+    "reduced-price": 1.3
+  },
+
+  "four-trip-ticket-ab": {
+    "name": "4-trip-ticket AB",
+    "zones": "AB",
+    "price": 9,
+    "reduced-price": 5.6
+  },
+  "four-trip-ticket-bc": {
+    "name": "4-trip-ticket BC",
+    "zones": "BC",
+    "price": 12,
+    "reduced-price": 8.4
+  },
+  "four-trip-ticket-abc": {
+    "name": "4-trip-ticket ABC",
+    "zones": "ABC",
+    "price": 13.2,
+    "reduced-price": 9.6
+  },
+
+  "day-ticket-ab": {
+    "name": "Day ticket AB",
+    "zones": "AB",
+    "price": 8.6,
+    "reduced-price": 5.5,
+    "days": 1
+  },
+  "day-ticket-abc": {
+    "name": "Day ticket ABC",
+    "zones": "ABC",
+    "price": 9.6,
+    "reduced-price": 6.5,
+    "days": 1
+  },
+
+  "seven-day-ticket-ab": {
+    "name": "Seven-Day ticket AB",
+    "zones": "AB",
+    "price": 34,
+    "days": 7
+  },
+  "seven-day-ticket-abc": {
+    "name": "Seven-Day ticket ABC",
+    "zones": "ABC",
+    "price": 41,
+    "days": 7
+  },
+
+  "city-tour-card-ab-48-hours": {
+    "name": "CityTourCard 48 hours AB",
+    "zones": "AB",
+    "price": 19.9,
+    "days": 2
+  },
+  "city-tour-card-abc-48-hours": {
+    "name": "CityTourCard 48 hours ABC",
+    "zones": "ABC",
+    "price": 22.9,
+    "days": 2
+  },
+  "city-tour-card-ab-72-hours": {
+    "name": "CityTourCard 72 hours AB",
+    "zones": "AB",
+    "price": 29.9,
+    "days": 3
+  },
+  "city-tour-card-abc-72-hours": {
+    "name": "CityTourCard 72 hours ABC",
+    "zones": "ABC",
+    "price": 33.9,
+    "days": 3
+  },
+  "city-tour-card-ab-4-days": {
+    "name": "CityTourCard 4 days AB",
+    "zones": "AB",
+    "price": 36.9,
+    "days": 4
+  },
+  "city-tour-card-abc-4-days": {
+    "name": "CityTourCard 4 days ABC",
+    "zones": "ABC",
+    "price": 41.9,
+    "days": 4
+  },
+  "city-tour-card-ab-5-days": {
+    "name": "CityTourCard 5 days AB",
+    "zones": "AB",
+    "price": 42.9,
+    "days": 5
+  },
+  "city-tour-card-abc-5-days": {
+    "name": "CityTourCard 5 days ABC",
+    "zones": "ABC",
+    "price": 46.9,
+    "days": 5
+  },
+  "city-tour-card-ab-6-days": {
+    "name": "CityTourCard 6 days AB",
+    "zones": "AB",
+    "price": 42.9,
+    "days": 6
+  },
+  "city-tour-card-abc-6-days": {
+    "name": "CityTourCard 6 days ABC",
+    "zones": "ABC",
+    "price": 46.9,
+    "days": 6
+  },
+  "welcome-card-ab-48-hours": {
+    "name": "WelcomeCard 48 hours AB",
+    "zones": "AB",
+    "price": 23,
+    "days": 2,
+    "guidebook": true
+  },
+  "welcome-card-abc-48-hours": {
+    "name": "WelcomeCard 48 hours ABC",
+    "zones": "ABC",
+    "price": 28,
+    "days": 2,
+    "guidebook": true
+  },
+  "welcome-card-ab-72-hours": {
+    "name": "WelcomeCard 72 hours AB",
+    "zones": "AB",
+    "price": 33,
+    "days": 3,
+    "guidebook": true
+  },
+  "welcome-card-abc-72-hours": {
+    "name": "WelcomeCard 72 hours ABC",
+    "zones": "ABC",
+    "price": 38,
+    "days": 3,
+    "guidebook": true
+  },
+  "welcome-card-ab-4-days": {
+    "name": "WelcomeCard 4 days AB",
+    "zones": "AB",
+    "price": 40,
+    "days": 4,
+    "guidebook": true
+  },
+  "welcome-card-abc-4-days": {
+    "name": "WelcomeCard 4 days ABC",
+    "zones": "ABC",
+    "price": 45,
+    "days": 4,
+    "guidebook": true
+  },
+  "welcome-card-ab-5-days": {
+    "name": "WelcomeCard 5 days AB",
+    "zones": "AB",
+    "price": 46,
+    "days": 5,
+    "guidebook": true
+  },
+  "welcome-card-abc-5-days": {
+    "name": "WelcomeCard 5 days ABC",
+    "zones": "ABC",
+    "price": 49,
+    "days": 5,
+    "guidebook": true
+  },
+  "welcome-card-ab-6-days": {
+    "name": "WelcomeCard 6 days AB",
+    "zones": "AB",
+    "price": 49,
+    "days": 6,
+    "guidebook": true
+  },
+  "welcome-card-abc-6-days": {
+    "name": "WelcomeCard 6 days ABC",
+    "zones": "ABC",
+    "price": 52,
+    "days": 6,
+    "guidebook": true
+  },
+  "welcome-card-ab-72-hours-museum-island": {
+    "name": "WelcomeCard 72 hours and Museum Island AB",
+    "zones": "AB",
+    "price": 51,
+    "days": 3,
+    "guidebook": true,
+    "museumIsland": true
+  },
+  "welcome-card-abc-72-hours-museum-island": {
+    "name": "WelcomeCard 72 hours and Museum Island ABC",
+    "zones": "ABC",
+    "price": 55,
+    "days": 3,
+    "guidebook": true,
+    "museumIsland": true
+  }
+}
