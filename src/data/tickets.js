e477ac600544b939b5d3defa8ae1658e727f3b20
Martin <hodovanmartin@gmail.com>
2020-11-16 12:44:27 +0100

    Fix ticket types data

--- src/data/tickets.js
+++ src/data/tickets.js
@@ -4,28 +4,32 @@ export const ticketTypes = [
     "zones": "AB",
     "price": 2.9,
     "reduced-price": 1.8,
-    "numberOfTrips": 1
+    "numberOfTrips": 1,
+    "guidebook": false
   },
   {
     "name": "Single ticket BC",
     "zones": "BC",
     "price": 3.3,
     "reduced-price": 2.3,
-    "numberOfTrips": 1
+    "numberOfTrips": 1,
+    "guidebook": false
   },
   {
     "name": "Single ticket ABC",
     "zones": "ABC",
     "price": 3.6,
     "reduced-price": 2.6,
-    "numberOfTrips": 1
+    "numberOfTrips": 1,
+    "guidebook": false
   },
   {
     "name": "Extension ticket A/C",
     "zones": "ABC",
     "price": 1.7,
     "reduced-price": 1.3,
-    "numberOfTrips": 1
+    "numberOfTrips": 1,
+    "guidebook": false
   },
 
   {
@@ -33,21 +37,24 @@ export const ticketTypes = [
     "zones": "AB",
     "price": 9,
     "reduced-price": 5.6,
-    "numberOfTrips": 4
+    "numberOfTrips": 4,
+    "guidebook": false
   },
   {
     "name": "4-trip-ticket BC",
     "zones": "BC",
     "price": 12,
     "reduced-price": 8.4,
-    "numberOfTrips": 4
+    "numberOfTrips": 4,
+    "guidebook": false
   },
   {
     "name": "4-trip-ticket ABC",
     "zones": "ABC",
     "price": 13.2,
     "reduced-price": 9.6,
-    "numberOfTrips": 4
+    "numberOfTrips": 4,
+    "guidebook": false
   },
 
   {
@@ -55,27 +62,31 @@ export const ticketTypes = [
     "zones": "AB",
     "price": 8.6,
     "reduced-price": 5.5,
-    "days": 1
+    "days": 1,
+    "guidebook": false
   },
   {
     "name": "Day ticket ABC",
     "zones": "ABC",
     "price": 9.6,
     "reduced-price": 6.5,
-    "days": 1
+    "days": 1,
+    "guidebook": false
   },
 
   {
     "name": "Seven-Day ticket AB",
     "zones": "AB",
     "price": 34,
-    "days": 7
+    "days": 7,
+    "guidebook": false
   },
   {
     "name": "Seven-Day ticket ABC",
     "zones": "ABC",
     "price": 41,
-    "days": 7
+    "days": 7,
+    "guidebook": false
   },
 
   {
@@ -96,49 +107,57 @@ export const ticketTypes = [
     "name": "CityTourCard 72 hours AB",
     "zones": "AB",
     "price": 29.9,
-    "days": 3
+    "days": 3,
+    "guidebook": false
   },
   {
     "name": "CityTourCard 72 hours ABC",
     "zones": "ABC",
     "price": 33.9,
-    "days": 3
+    "days": 3,
+    "guidebook": false
   },
   {
     "name": "CityTourCard 4 days AB",
     "zones": "AB",
     "price": 36.9,
-    "days": 4
+    "days": 4,
+    "guidebook": false
   },
   {
     "name": "CityTourCard 4 days ABC",
     "zones": "ABC",
     "price": 41.9,
-    "days": 4
+    "days": 4,
+    "guidebook": false
   },
   {
     "name": "CityTourCard 5 days AB",
     "zones": "AB",
     "price": 42.9,
-    "days": 5
+    "days": 5,
+    "guidebook": false
   },
   {
     "name": "CityTourCard 5 days ABC",
     "zones": "ABC",
     "price": 46.9,
-    "days": 5
+    "days": 5,
+    "guidebook": false
   },
   {
     "name": "CityTourCard 6 days AB",
     "zones": "AB",
     "price": 42.9,
-    "days": 6
+    "days": 6,
+    "guidebook": false
   },
   {
     "name": "CityTourCard 6 days ABC",
     "zones": "ABC",
     "price": 46.9,
-    "days": 6
+    "days": 6,
+    "guidebook": false
   },
   {
     "name": "WelcomeCard 48 hours AB",
